266bd23d2aa8d93dafa5e9c2749425c3059668ff
BVargasTapias <brayanvargas0710@gmail.com>
2025-02-12 16:06:56 -0500

    actualizacion back

---
+++ backend/auth/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, jsonify
+from models import db, User
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token
+
+auth_bp = Blueprint('auth', __name__)
+bcrypt = Bcrypt()
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username', '')
+    password = data.get('password', '')
+
+    user = User.query.filter_by(username=username).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity={'id': user.id, 'username': user.username})
+        return jsonify(access_token=access_token), 200
+    return jsonify(message="Invalid credentials"), 401
